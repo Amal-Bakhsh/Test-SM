6582f02938bb1e15a5b81a8aed690a74bb026340
Amal-Bakhsh <121556137+Amal-Bakhsh@users.noreply.github.com>
2024-03-07 21:13:19 +0300

    Add files via upload

---
+++ ServicesConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.EntityConfigurations
+{
+    class ServicesConfiguration : IEntityTypeConfiguration<Services>
+    {
+        public void Configure(EntityTypeBuilder<Services> builder)
+        {
+            builder.ToTable("Services");
+
+            builder.HasKey(p => p.Id);
+            builder
+                .Property(p => p.ClientId)
+                .IsRequired();
+
+
+
+        }
+
+    }
+}
