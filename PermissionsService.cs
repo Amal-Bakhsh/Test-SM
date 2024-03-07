6848c48fd81d9cde488c62324fb4bb388bc4e6ff
Amal-Bakhsh <121556137+Amal-Bakhsh@users.noreply.github.com>
2024-03-07 21:10:41 +0300

    Add files via upload

---
+++ PermissionsService.cs
@@ -0,0 +1,102 @@
+﻿using Api.Models;
+using AutoMapper;
+using DataAccessLayer.Entities;
+using DataAccessLayer.Context;
+using DataAccessLayer.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using DataAccessLayer.Interfaces;
+using System.Text.Json;
+
+namespace Api.Services
+{
+    public class PermissionsService
+    {
+        private readonly IMapper _mapper;
+        private ICurrentUserService _currentUserService;
+        private IRepository<PermissionsGroup> _repository;
+        private IRepository<PermissionsCategory> _PermissionsCategoryRepository;
+        private IRepository<Permissions> _PermissionsRepository;
+        private IRepository<User> _UserRepository;
+        private readonly SmartmenuDbContext _context;
+
+        public PermissionsService(IMapper mapper, ICurrentUserService currentUserService, SmartmenuDbContext context)
+        {
+            _mapper = mapper;
+            _currentUserService = currentUserService;
+            _context = context;
+        }
+
+
+        public async Task<List<PermissionsCategoryVM>> GetAllCategoriesAsync()
+        {
+            _PermissionsCategoryRepository = new PermissionsCategoryRepository(_context);
+            List<PermissionsCategory> Category = await _PermissionsCategoryRepository.GetAllAsync().Result.ToListAsync();
+            List<PermissionsCategoryVM> CategoryVM1 = _mapper.Map<List<PermissionsCategoryVM>>(Category);
+            return CategoryVM1;
+        }
+
+        public async Task<List<PermissionsVM>> GetAllPermissionsAsync()
+        {
+            _PermissionsRepository = new PermissionsRepository(_context);
+            List<Permissions> Permissions = await _PermissionsRepository.GetAllAsync().Result.ToListAsync();
+            List<PermissionsVM> PermissionsVM1 = _mapper.Map<List<PermissionsVM>>(Permissions);
+            return PermissionsVM1;
+        }
+
+        public async Task<UserVM> GetGroupById(int id)
+        {
+            _repository = new PermissionsGroupRepository(_context);
+            _UserRepository = new UserRepository(_context);
+            User user = _UserRepository.GetAsync(id).Result;
+            PermissionsGroup Group = await _repository.GetbyFilterAsync(a => a.Id == user.PermissionGroupId).Result.FirstOrDefaultAsync();
+            PermissionsGroupVM GroupVM1 = _mapper.Map<PermissionsGroupVM>(Group);
+            UserVM userVM = _mapper.Map<UserVM>(user);
+            userVM.PermissionsGroup = GroupVM1;
+            return userVM;
+        }
+
+        public async Task<UserVM> UpdatPG(UserVM userVM)
+        {
+            _repository = new PermissionsGroupRepository(_context);
+            _UserRepository = new UserRepository(_context);
+            User user = _UserRepository.GetAsync(userVM.Id).Result;
+            PermissionsGroup Group = _mapper.Map<PermissionsGroup>(userVM.PermissionsGroup);
+            if(Group.Name == "Admin" || Group.Name == "Default" 
+                || Group.Name == "Operation" || Group.Name == "Waiter"
+                || Group.Name == "Supervisor" || Group.Name == "Maintenance"
+                 || Group.Name == "Inspector" || Group.Name == "Service")
+            {
+                Group.Id = 0;
+                Group.Name = userVM.UserName + "PG";
+                await _repository.AddAsync(Group);
+                await _repository.SaveChangesAsync();
+                user.PermissionGroupId = Group.Id;
+                await _UserRepository.UpdateAsync(user);
+                await _UserRepository.SaveChangesAsync();
+            }
+            else
+            {
+                await _repository.UpdateAsync(Group);
+                await _repository.SaveChangesAsync();
+            }
+            PermissionsGroupVM GroupVM1 = _mapper.Map<PermissionsGroupVM>(Group);
+            UserVM userVM1 = _mapper.Map<UserVM>(user);
+            userVM1.PermissionsGroup = GroupVM1;
+            return userVM1;
+        }
+
+        public async Task<List<PermissionsGroupVM>> GetinternalUsersPG()
+        {
+            _repository = new PermissionsGroupRepository(_context);
+            _UserRepository = new UserRepository(_context);
+            List<PermissionsGroup> Group = await _repository.GetbyFilterAsync(a => a.Name == "Operation" || a.Name == "Waiter" || a.Name == "Supervisor" || a.Name== "Inspector" || a.Name== "Maintenance" || a.Name == "Service").Result.ToListAsync();
+            List<PermissionsGroupVM> GroupVM1 = _mapper.Map<List<PermissionsGroupVM>>(Group);
+            return GroupVM1;
+        }
+    }
+}
---
+++ ServicesService.cs
@@ -0,0 +1,221 @@
+﻿using Api.Models;
+using AutoMapper;
+using DataAccessLayer.Entities;
+using DataAccessLayer.Context;
+using DataAccessLayer.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using DataAccessLayer.Interfaces;
+
+namespace Api.Services
+{
+    public class ServicesService
+    {
+        private readonly IMapper _mapper;
+        private ICurrentUserService _currentUserService;
+        private IRepository<DataAccessLayer.Entities.Services> _repository;
+        private readonly SmartmenuDbContext _context;
+        private IRepository<DataAccessLayer.Entities.UserServicesMapping> _repositoryMapping;
+        private IRepository<Client> _clientRepository;
+        private IRepository<Hotel> _hotelRepository;
+        private IRepository<User> _userRepository;
+        private IRepository<ClientServices> _ClientServicesRepository;
+        private IRepository<UserClientMapping> _UserClientMappingRepository;
+
+		public ServicesService(IMapper mapper, ICurrentUserService currentUserService, SmartmenuDbContext context)
+        {
+            _mapper = mapper;
+            _currentUserService = currentUserService;
+            _context = context;
+        }
+
+        public async Task<ServicesVM> AddAsync(ServicesVM ServicesVM)
+        {
+            _repository = new ServicesRepository(_context);
+           // _clientRepository = new ClientRepository(_context);
+            _clientRepository = new ClientRepository(_context);
+            ServicesVM ServicesVM1 = null;
+            Client client = _clientRepository.GetAsync(ServicesVM.ClientId).Result;
+            if (client.MaxService > client.ServiceNum)
+            {
+                _userRepository = new UserRepository(_context);
+                _ClientServicesRepository = new ClientServicesRepository(_context);
+                _userRepository = new UserRepository(_context);
+                _repositoryMapping = new UserServicesMappingRepository(_context);
+                DataAccessLayer.Entities.Services services = _mapper.Map<DataAccessLayer.Entities.Services>(ServicesVM);
+                await _repository.AddAsync(services);
+                await _repository.SaveChangesAsync();
+				client.ServiceNum++;
+                await _clientRepository.UpdateAsync(client);
+                await _clientRepository.SaveChangesAsync();
+                Client currentClient = _currentUserService.Client != null ? _currentUserService.Client : await _clientRepository.GetAsync(ServicesVM.ClientId);
+                int serviceId = services.Id;
+                DataAccessLayer.Entities.ClientServices clientService = new ClientServices();
+                clientService.ServicesId = serviceId;
+                clientService.ClientId = currentClient.Id;
+                clientService.Status = true;
+                await _ClientServicesRepository.AddAsync(clientService);
+                await _ClientServicesRepository.SaveChangesAsync();
+				//DataAccessLayer.Entities.UserServicesMapping userServiceMapping = new UserServicesMapping();
+    //            userServiceMapping.ServicesId = serviceId;
+    //            userServiceMapping.UserId = _currentUserService.User.Id;
+    //            userServiceMapping.Status = true;
+    //            await _repositoryMapping.AddAsync(userServiceMapping);
+    //            await _repositoryMapping.SaveChangesAsync();
+
+    //            if (currentClient.MainBranchId == 0)
+    //            {
+    //                List<Client> clients = await _clientRepository.GetbyFilterAsync(a => a.MainBranchId == currentClient.Id).Result.ToListAsync();
+    //                foreach (Client clien in clients)
+    //                {
+    //                    ClientServices clientServices2 = new ClientServices();
+    //                    clientServices2.ServicesId = serviceId;
+    //                    clientServices2.ClientId = client.Id;
+    //                    clientServices2.Status = true;
+    //                    await _ClientServicesRepository.AddAsync(clientServices2);
+    //                    await _ClientServicesRepository.SaveChangesAsync();
+    //                }
+    //            }
+                ServicesVM1 = _mapper.Map<ServicesVM>(services);
+            }
+            return ServicesVM1;
+        }
+
+        public async Task<UserServicesMappingVM> AddUserServiceMapping(UserServicesMappingVM userServiceMappingVM)
+        {
+            _repositoryMapping = new UserServicesMappingRepository(_context);
+            _repository = new ServicesRepository(_context);
+            //_ClientCategoryRepository = new ClientCategoryRepository(_context);
+            UserServicesMapping userMapping = _mapper.Map<UserServicesMapping>(userServiceMappingVM);
+            await _repositoryMapping.AddAsync(userMapping);
+            await _repositoryMapping.SaveChangesAsync();
+            UserServicesMappingVM servicesMappingVM = _mapper.Map<UserServicesMappingVM>(userMapping);
+            return servicesMappingVM;
+
+
+        }
+
+        public async Task<ServicesVM> UpdateAsync(ServicesVM ServicesVM)
+        {
+            _repository = new ServicesRepository(_context);
+            DataAccessLayer.Entities.Services services = _mapper.Map<DataAccessLayer.Entities.Services>(ServicesVM);
+            services.LastModifiedBy = Convert.ToString(_currentUserService?.User?.Id);
+            services.LastModifiedOn = DateTime.Now;
+            await _repository.UpdateAsync(services);
+            await _repository.SaveChangesAsync();
+            ServicesVM ServicesVM1 = _mapper.Map<ServicesVM>(services);
+            return ServicesVM1;
+        }
+
+        public async Task<ServicesVM> DeleteAsync(ServicesVM ServicesVM)
+        {
+            _repository = new ServicesRepository(_context);
+            DataAccessLayer.Entities.Services services = _mapper.Map<DataAccessLayer.Entities.Services>(ServicesVM);
+            services.LastModifiedBy = Convert.ToString(_currentUserService?.User?.Id);
+            services.LastModifiedOn = DateTime.Now;
+            await _repository.DeleteAsync(services);
+            await _repository.SaveChangesAsync();
+            ServicesVM ServicesVM1 = _mapper.Map<ServicesVM>(services);
+
+            return ServicesVM1;
+        }
+
+        public async Task<ServicesVM> GetAsync(int id)
+        {
+            _repository = new ServicesRepository(_context);
+            DataAccessLayer.Entities.Services services = await _repository.GetAsync(id);
+            ServicesVM ServicesVM1 = _mapper.Map<ServicesVM>(services);
+
+            return ServicesVM1;
+        }
+
+        public async Task<List<ClientServicesVM>> GetAsyncByClient(int id)
+        {
+            //_repository = new ServicesRepository(_context);
+            //List<DataAccessLayer.Entities.Services> services = await _repository.GetbyFilterAsync(a => a.ClientId == clientId).Result.ToListAsync();
+            ////List<Theme> Theme = await _repository.GetbyFilterAsync(a => a.CategoryId == categoryid).Result.ToListAsync();
+            //List<ServicesVM> ServicesVM1 = _mapper.Map<List<ServicesVM>>(services);
+
+            //return ServicesVM1;
+            _repository = new ServicesRepository(_context);
+            _ClientServicesRepository = new ClientServicesRepository(_context);
+            List<ClientServices> cServices = await _ClientServicesRepository
+                .GetbyFilterAsync(a => a.ClientId == id
+                 && a.Services.IsShown)
+                .Result.Include(a => a.Services.Labels)
+                .OrderBy(a => a.Services.Priority)
+                .ToListAsync();
+            List<ClientServicesVM> cServiceVM = _mapper.Map<List<ClientServicesVM>>(cServices);
+            return cServiceVM;
+        }
+
+        public async Task<List<ServicesVM>> GetAllAsync()
+        {
+            _repository = new ServicesRepository(_context);
+            List<DataAccessLayer.Entities.Services> services = await _repository.GetAllAsync().Result.ToListAsync();
+            List<ServicesVM> ServicesVM1 = _mapper.Map<List<ServicesVM>>(services);
+
+            return ServicesVM1;
+        }
+
+        public async Task<List<ServicesVM>> GetbyFilterAsync(Expression<Func<DataAccessLayer.Entities.Services, bool>> condition)
+        {
+            _repository = new ServicesRepository(_context);
+            List<DataAccessLayer.Entities.Services> services = await _repository.GetbyFilterAsync(condition).Result.ToListAsync();
+            List<ServicesVM> ServicesVM1 = _mapper.Map<List<ServicesVM>>(services);
+            return ServicesVM1;
+        }
+
+        public async Task<ClientServicesVM> UpdateStatus(ClientServicesVM ServicesVM)
+        {
+            _repository = new ServicesRepository(_context);
+            _ClientServicesRepository = new ClientServicesRepository(_context);
+            _repositoryMapping = new UserServicesMappingRepository(_context);
+            _userRepository = new UserRepository(_context);
+
+            ClientServices clientServices = _mapper.Map<ClientServices>(ServicesVM);
+            await _ClientServicesRepository.UpdateAsync(clientServices);
+            await _ClientServicesRepository.SaveChangesAsync();
+            
+            UserServicesMapping cServices = await _repositoryMapping.GetbyFilterAsync(a => a.ServicesId == ServicesVM.ServicesId).Result.FirstOrDefaultAsync();
+            
+            if (cServices != null)
+            {
+                cServices.Status = ServicesVM.Status;
+                await _repositoryMapping.UpdateAsync(cServices);
+                await _repositoryMapping.SaveChangesAsync();
+                User user = await _userRepository.GetbyFilterAsync(a => a.Id == cServices.UserId).Result.FirstOrDefaultAsync();
+                user.Status = ServicesVM.Status;
+                await _userRepository.UpdateAsync(user);
+                await _userRepository.SaveChangesAsync();
+            }
+
+
+            ClientServicesVM ServicesVM1 = _mapper.Map<ClientServicesVM>(clientServices);
+            return ServicesVM1;
+        }
+
+        public async Task<UserServicesMappingVM> GetServiceByUser(int id)
+        {
+            //_repository = new ServicesRepository(_context);
+            //List<DataAccessLayer.Entities.Services> services = await _repository.GetbyFilterAsync(a => a.ClientId == clientId).Result.ToListAsync();
+            ////List<Theme> Theme = await _repository.GetbyFilterAsync(a => a.CategoryId == categoryid).Result.ToListAsync();
+            //List<ServicesVM> ServicesVM1 = _mapper.Map<List<ServicesVM>>(services);
+
+            //return ServicesVM1;
+            _repositoryMapping = new UserServicesMappingRepository(_context);
+            UserServicesMapping cServices = await _repositoryMapping
+                .GetbyFilterAsync(a => a.UserId == id)
+                .Result.Include(a => a.Services.Labels)
+                .OrderBy(a => a.Services.Priority)
+                .FirstOrDefaultAsync();
+            UserServicesMappingVM cServiceVM = _mapper.Map<UserServicesMappingVM>(cServices);
+            return cServiceVM;
+        }
+
+    }
+}
